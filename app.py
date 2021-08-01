f7e44aa3abda2f60742fe3e81b7c39de7e71666a
bulidiriba <bulidiriba@gmail.com>
2021-08-01 08:46:14 +0300

    add the download or send file method by getting the path where the file exists as arguments

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, flash, redirect, url_for
+from flask import Flask, request, jsonify, flash, send_file
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
@@ -22,35 +22,32 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/upload_files', methods=['GET','POST'])
 def upload_files():
     print("-------upload files upload files-----------")
-    result = "Successful"
     if request.method == 'POST':
         # check if the post has request has the file
         if 'file1' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        
         file1 = request.files['file1']
         file2 = request.files['file2']
-        print("file 1", file1)
-        print("file 2", file2)
-
         if file1 and file2:
             filename1 = secure_filename(file1.filename)
             filename2 = secure_filename(file2.filename)
-            
-            print("filename1: ", filename1)
-            print("filename2: ", filename2)
-
             file1.save(os.path.join(UPLOAD_FOLDER, filename1))
             file2.save(os.path.join(UPLOAD_FOLDER, filename2))
-
             result = execute(os.path.join(UPLOAD_FOLDER, filename1), os.path.join(UPLOAD_FOLDER, filename2))
-            print("------result-------", result)
-
-            return result
+            
+            return jsonify(result)
 
     return
 
+@app.route('/download_file', methods=['GET'])
+def download_file():
+    file_path = request.args.get("file_path")
+    print("----------file path file path------", file_path)
+    return send_file(
+            file_path,
+            as_attachment = True
+        )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
